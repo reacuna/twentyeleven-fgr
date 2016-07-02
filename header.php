0cdb48ab345cd3e7d266b85d94c733f548a44e02
Raúl Acuña <reacuna@crac.cl>
2016-07-02 18:15:15 -0400

    Set initial scale on viewport header

--- header.php
+++ header.php
@@ -23,7 +23,8 @@
 <!--<![endif]-->
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
-<meta name="viewport" content="width=device-width" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title><?php
 	// Print the <title> tag based on what is being viewed.
 	global $page, $paged;
